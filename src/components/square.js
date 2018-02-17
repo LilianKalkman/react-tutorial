e66437b10f0930742ee9c3fb6806277054851088
LilianKalkman <liliankalkman@hotmail.com>
2018-02-17 11:54:35 +0100

    state to board

--- src/components/square.js
+++ src/components/square.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
-  constructor(props){
-    super(props);
-    this.state = { value: null };
-  }
-
-  onSquareClick(event){
-    this.setState({value: 'X'});
-  }
-
   render() {
     return (
-      // <button className="square" onClick={()=> this.setState({value: 'X'})}>
-      <button className="square" onClick={this.onSquareClick.bind(this)}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
+
 
 export default Square;
