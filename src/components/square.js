c20cf76335e104870c2178cf8eddf47ea9430931
LilianKalkman <liliankalkman@hotmail.com>
2018-02-17 11:24:15 +0100

    xjes

--- src/components/square.js
+++ src/components/square.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+  constructor(props){
+    super(props);
+    this.state = { value: null };
+  }
+
+  onSquareClick(event){
+    this.setState({value: 'X'});
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      // <button className="square" onClick={()=> this.setState({value: 'X'})}>
+      <button className="square" onClick={this.onSquareClick.bind(this)}>
+        {this.state.value}
       </button>
     );
   }
 }
 
-export default Square; 
+export default Square;
